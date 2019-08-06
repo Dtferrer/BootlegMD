513d2ec5c6dbc3110a6ea79443d49ec1d5090006
Danica Ferrer <dtferrer@ucsd.edu>
2019-08-05 20:35:04 -0700

    Fixed Navbar and Added Array for URL Search

--- SymptomsPage/symptomsPage.js
+++ SymptomsPage/symptomsPage.js
@@ -1,6 +1,10 @@
+var listSymp = [];
+
 $(".add").on("click", function() {
     var symptom = $("#inputSymptom").val();
     console.log(symptom);
+    listSymp.push(symptom); 
+    console.log(listSymp);
     var newsymp = $("<button type='button' class='btn addSymp' style='background-color:#F8FDFF; margin: 0px 5px'>" + symptom + "</button></li>");
     $(".AddedSymptoms").append(newsymp);    
 });
