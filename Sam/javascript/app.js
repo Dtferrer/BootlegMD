0fd80ce1d0baf9de71ca106d454f77e0928ee890
Sam Pai <sampai@Sams-MacBook-Pro.local>
2019-08-07 19:42:46 -0700

    added functionality

--- Sam/javascript/app.js
+++ Sam/javascript/app.js
@@ -1,23 +1,38 @@
-// function test(){
-//     var queryUrl: 'http://open.mapquestapi.com/directions/v2/route?key=A6vc2e1WIOf1eHAai2WljNdGvoRQAqi1&from=Clarendon Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA';
-
-
-//     $.ajax({
-//         url:url,
-//         method:"GET"
-//     }).then(function(response) {
-//         console.log(response);
-//     })
+function test(){
     
-// }
-// test();
+    
+    var queryUrl = 'http://open.mapquestapi.com/directions/v2/route?key=A6vc2e1WIOf1eHAai2WljNdGvoRQAqi1&from=2000 Clarendon Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA';
+
+
+    $.ajax({
+        url: queryUrl,
+        method:"GET"
+    }).then(function(response) {
+        var res = response.route.legs[0].maneuvers;
+        // console.log(response.route.legs)
+        
+        var res2 = response.route.legs;
+        for (let j = 0; j < res2.length; j++) {
+            // console.log(res2[j]);
+            for (let i = 0; i < res.length; i++) {
+                console.log(res[i].narrative);
+                
+            }
+        }
+
+        
+    })
+    
+}
+test();
 
 //get URL grab response
 
 
 
 $("#submit").on("click", function(){
-    var start = $("#exampleInputlocation1").val().trim();
-    console.log("ADDRESS");
+   var startLocation = $("#exampleInputlocation1").val().trim();
+   var endLocation = $("exampleInputlocation2").val().trim();
+   
 })
 
