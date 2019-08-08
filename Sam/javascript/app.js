0426d4d4540a343404180a5427b7d4dc5d79e183
Sam Pai <sampai@Sams-MacBook-Pro.local>
2019-08-07 18:33:12 -0700

    added frame work and a little css styling

---
+++ Sam/javascript/app.js
@@ -0,0 +1,23 @@
+// function test(){
+//     var queryUrl: 'http://open.mapquestapi.com/directions/v2/route?key=A6vc2e1WIOf1eHAai2WljNdGvoRQAqi1&from=Clarendon Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA';
+
+
+//     $.ajax({
+//         url:url,
+//         method:"GET"
+//     }).then(function(response) {
+//         console.log(response);
+//     })
+    
+// }
+// test();
+
+//get URL grab response
+
+
+
+$("#submit").on("click", function(){
+    var start = $("#exampleInputlocation1").val().trim();
+    console.log("ADDRESS");
+})
+
